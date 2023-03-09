9e81f96049bd0cfb0e0205938b08197df490f61e
CraigFalasco <craigfalasco>
2023-03-09 14:22:50 -0700

    add ZStack and background color

--- BaseNumbers/ContentView.swift
+++ BaseNumbers/ContentView.swift
@@ -17,59 +17,65 @@ struct ContentView: View {
     let baseValues =  [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
     
     var body: some View {
-        
-        Form {
-            
-            Text("Convert decimal to any base")
-                .font(.title2)
-                .bold()
-            
-            Section(header: Text("YOUR NUMBER AND BASE")) {
+
+        ZStack {
+            Color.pink
+                .ignoresSafeArea()
+            Form {
                 
-                HStack {
-                    Text("Number:")
-                    TextField("", text: $number, prompt: Text("number up to 18 digits"))
-                }
+                Text("Convert decimal to any base")
+                    .font(.title2)
+                    .bold()
                 
-                if checkNumber() || number.isEmpty {
-                    Text("")
-                }
-                else {
-                    Text("(must be numeric and up to 18 digits)")
-                        .font(.subheadline)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.red)
-                }
-                
-                HStack {
-                    Picker("Base", selection: $base) {
-                        ForEach(0..<baseValues.count, id: \.self) { index in
-                            Text(String(baseValues[index])).tag(String(index + 2))
+                Section(header: Text("YOUR NUMBER AND BASE")) {
+                    
+                    HStack {
+                        Text("Number:")
+                        TextField("", text: $number, prompt: Text("number up to 18 digits"))
+                    }
+                    
+                    if checkNumber() || number.isEmpty {
+                        Text("")
+                    }
+                    else {
+                        Text("(must be numeric and up to 18 digits)")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.red)
+                    }
+                    
+                    HStack {
+                        Picker("Base", selection: $base) {
+                            ForEach(0..<baseValues.count, id: \.self) { index in
+                                Text(String(baseValues[index])).tag(String(index + 2))
+                            }
                         }
                     }
+                    
+                    if checkNumber() {
+                        Button("SUBMIT") {
+                            intNum = Int(number) ?? 1
+                            intBase = Int(base) ?? 10
+                            result = convertNumber(inNum: intNum, inBase: intBase)
+                        }
+                        .padding()
+                        .background(Color(.red))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                    }
                 }
                 
-                if checkNumber() {
-                    Button("SUBMIT") {
-                        intNum = Int(number) ?? 1
-                        intBase = Int(base) ?? 10
-                        result = convertNumber(inNum: intNum, inBase: intBase)
-                    }
-                    .padding()
-                    .background(Color(.red))
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                Section(header: Text("RESULT")) {
+                    Text("\(intNum) in base \(intBase) is:")
+                        .bold()
+                    Text(result.uppercased())
+                        .bold()
+                        .font(.title2)
                 }
             }
-            Section(header: Text("RESULT")) {
-                Text("\(intNum) in base \(intBase) is:")
-                    .bold()
-                Text(result.uppercased())
-                    .bold()
-                    .font(.title2)
-            }
+            .padding()
+            .cornerRadius(10)
         }
-        .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
     }
     
     func convertNumber(inNum: Int, inBase: Int) -> String {
